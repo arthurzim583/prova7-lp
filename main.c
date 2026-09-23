d1994bc74d59e98af9d66bf49c83458795ee64f4
arthurzim583 <joaoarthurbrumpontes@gmail.com>
2026-09-22 22:05:48 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,65 +1,52 @@
 #include <stdio.h>
 
-void main() {
+void main()
+{
 
-float nota1;
-float nota2;
-float nota3;
-float nota4;
-char aluno;
-float media;
+    float nota1;
+    float nota2;
+    float nota3;
+    float nota4;
+    char aluno;
+    float media;
 
+    printf("/n digite o nome do aluno");
+    scanf("%s", aluno);
 
+    printf("/n digite a primeira nota");
+    scanf("%f", nota1);
 
-printf("/n digite o nome do aluno");
-scanf("%s" , aluno);
+    printf("/n digite a segunda nota");
+    scanf("%f", nota2);
 
+    printf("/n digite a terceira nota");
+    scanf("%f", nota3);
 
-printf("/n digite a primeira nota");
-scanf("%f" , nota1);
+    printf("/n digite a quarta nota");
+    scanf("%f", nota4);
 
+    media == nota1 + nota2 + nota3 + nota4 / 4
 
-printf("/n digite a segunda nota");
-scanf("%f" , nota2);
+    if ()
+    {
+        media < 5, 0;
+        printf("reprovado");
+    }
 
+    else
+    {
+        media > 7, 0;
+        printf("aprovado");
+    }
 
-printf("/n digite a terceira nota");
-scanf("%f" , nota3);
+    while ()
+    {
+        media == 5, 0;
+        printf("recuperacao");
 
+        break;
 
-printf("/n digite a quarta nota");
-scanf("%f" , nota4);
-
-media == nota1 + nota2 + nota3 + nota4 / 4;
-
-
-if {
-media < 5,0;
-printf("reprovado");
-
+        printf("deseja cadastrar outro aluno?");
+        scanf("%s", aluno);
+    }
 }
-
-else{
-media > 7,0;
-printf("aprovado");
-
-
-}
-
-while{
-media == 5,0;
-printf("recuperacao");
-
-break;
-
-printf("deseja cadastrar outro aluno?");
-scanf("%s" , aluno);
-
-
-}
-
-
-return:0
-
-
-} 
