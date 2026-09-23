4d0e295b03919bee9e5f43c2e36245a8c8507a70
arthurzim583 <joaoarthurbrumpontes@gmail.com>
2026-09-22 21:51:01 -0300

    .

---
+++ prova7.lp/prova7.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+
+void boletim() {
+
+float nota1;
+float nota2;
+float nota3;
+float nota4;
+char aluno;
+float media;
+
+
+
+printf("/n digite o nome do aluno");
+scanf("%s" , aluno);
+
+
+printf("/n digite a primeira nota");
+scanf("%f" , nota1);
+
+
+printf("/n digite a segunda nota");
+scanf("%f" , nota2);
+
+
+printf("/n digite a terceira nota");
+scanf("%f" , nota3);
+
+
+printf("/n digite a quarta nota");
+scanf("%f" , nota4);
+
+media == nota1 + nota2 + nota3 + nota4 / 4;
+
+
+if {
+media < 5,0;
+printf("reprovado");
+
+}
+
+else{
+media > 7,0;
+printf("aprovado");
+
+
+}
+
+while{
+media == 5,0;
+printf("recuperacao");
+
+break;
+
+printf("deseja cadastrar outro aluno?");
+scanf("%s" , aluno);
+
+
+}
+
+
+return:0
+
+
+} 
